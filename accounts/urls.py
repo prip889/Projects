4a63190c73d80a74e3d6dedd5d47124c4c5d2468
Priya <pxp405@case.edu>
2023-06-05 18:40:36 -0400

    Feature 9 done

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
-from .views import user_login
+from .views import user_login, user_logout
 from django.urls import path
 
 urlpatterns = [
     path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
 
 ]
