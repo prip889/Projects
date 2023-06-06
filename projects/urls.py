aa708caa99e8d4bb614cd6a9fd776918776ade41
Priya <pxp405@case.edu>
2023-06-06 18:25:00 -0400

    Feature 13 done

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import project_list
+from .views import project_list, project_detail
 
 urlpatterns = [
     path("", project_list, name="list_projects"),
+    path("<int:id>/", project_detail, name="show_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -10,3 +10,12 @@ def project_list(request):
         "projects": item,
     }
     return render(request, "projects/list.html", context)
+
+
+@login_required
+def project_detail(request, id):
+    project = Project.objects.get(id=id)
+    context = {
+        "project": project
+    }
+    return render(request, "projects/detail.html", context)
